27e56cce79d77e277817a2cdeff437aff52249f4
jewstinbeac <102846792+jewstinbeac@users.noreply.github.com>
2022-04-05 15:55:27 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 from mpi4py import MPI
 import time
 
-with open('sydGrid-2.json') as json_file:
+with open('sydGrid.json') as json_file:
     grid = json.load(json_file)
 
 start_time = time.time()
@@ -17,13 +17,16 @@ location = defaultdict(lambda: defaultdict(int))
 
 
 def process_tweet(tweet):
-    ptweet = tweet[:-2].removesuffix("]")
+    ptweet = tweet[:-2]
+    if ptweet[-1] == ']':
+        ptweet = ptweet[:-1]
     tweet_json = json.loads(ptweet)
+    id = None
+    lang = None
     if tweet_json['doc']['coordinates'] is not None:
         x = tweet_json['doc']['coordinates']['coordinates'][0]
         y = tweet_json['doc']['coordinates']['coordinates'][1]
         lang = tweet_json['doc']['lang']
-        id = None
 
         for box in grid['features']:
             if x >= box['geometry']['coordinates'][0][0][0] and x < box['geometry']['coordinates'][0][2][0]:
@@ -52,7 +55,7 @@ if rank == 0:
 
     twts.close()
 
-    for i in size:
+    for i in range(size):
         comm.send('FINISH', dest=i)
 
 else:
@@ -72,4 +75,17 @@ else:
         if finish:
             break
 
-print(location)
+comm.barrier()
+gather_data = comm.gather(location, root=0)
+
+
+if rank == 0:
+    final_data = gather_data[0]
+
+    for dict in gather_data[1:]:
+        for id in dict:
+            for lang in id:
+                final_data[id][lang] += dict[id][lang]
+
+print(final_data)
+
