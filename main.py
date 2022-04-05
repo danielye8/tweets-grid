0f17ef2801368854a25dfdc348ad7eace214d5ea
jewstinbeac <102846792+jewstinbeac@users.noreply.github.com>
2022-04-05 14:33:16 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from collections import defaultdict
 from mpi4py import MPI
 import time
 
-
-with open('/Users/justinbeaconsfield/Downloads/sydGrid-2.json') as json_file:
+with open('sydGrid-2.json') as json_file:
     grid = json.load(json_file)
 
 start_time = time.time()
@@ -14,14 +13,41 @@ comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-tweets = []
 location = defaultdict(lambda: defaultdict(int))
 
+
+def process_tweet(tweet):
+    ptweet = tweet[:-2].removesuffix("]")
+    tweet_json = json.loads(ptweet)
+    if tweet_json['doc']['coordinates'] is not None:
+        x = tweet_json['doc']['coordinates']['coordinates'][0]
+        y = tweet_json['doc']['coordinates']['coordinates'][1]
+        lang = tweet_json['doc']['lang']
+        id = None
+
+        for box in grid['features']:
+            if x >= box['geometry']['coordinates'][0][0][0] and x < box['geometry']['coordinates'][0][2][0]:
+                if y >= box['geometry']['coordinates'][0][2][1] and y < box['geometry']['coordinates'][0][0][1]:
+                    id = box['properties']['id']
+                    break
+
+    return id, lang
+
+
 if rank == 0:
-    with open('/Users/justinbeaconsfield/Downloads/tinyTwitter.json') as twts:
+    tweets = []
+    with open('tinyTwitter.json') as twts:
         i = 0
         for tweet in twts:
-            comm.send(tweet, dest= i%size)
+            if i % size != 0:
+                comm.send(tweet, dest=i % size)
+            else:
+                if tweet[2:7] != "total":
+                    id, lang = process_tweet(tweet)
+
+                    if id is not None:
+                        location[id][lang] += 1
+
             i += 1
 
     twts.close()
@@ -32,30 +58,18 @@ if rank == 0:
 else:
     finish = False
     while True:
-        tweets.append(comm.recv(source=0))
-        for tweet in tweets:
-            if tweet == 'FINISH':
-                finish = True
-                break
+        tweet = comm.recv(source=0)
+        if tweet == 'FINISH':
+            finish = True
+            break
 
-            if tweet[2:7] != "total":
-                ptweet = tweet[:-2].removesuffix("]")
-                if json.loads(ptweet)['doc']['coordinates'] is not None:
-                    x = tweet['doc']['coordinates']['coordinates'][0]
-                    y = tweet['doc']['coordinates']['coordinates'][1]
-                    lang = tweet['doc']['lang']
-                    id = None
+        if tweet[2:7] != "total":
+            id, lang = process_tweet(tweet)
 
-                    for box in grid['features']:
-                        if x >= box['geometry']['coordinates'][0][0][0] and x < box['geometry']['coordinates'][0][2][0]:
-                            if y >= box['geometry']['coordinates'][0][2][1] and y < box['geometry']['coordinates'][0][0][1]:
-                                id = box['properties']['id']
-                                break
-
-                    if id is not None:
-                        location[id][lang] += 1
-
-            tweets.pop(tweet)
+            if id is not None:
+                location[id][lang] += 1
 
         if finish:
             break
+
+print(location)
